5661d0ec94ee63620d0e3c724555a4ab69a894a5
marcos-dev14 <mprogramador.10@gmail.com>
2024-06-08 20:05:17 -0300

    feat: adicionar estratégia de layouts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { AppLayout } from './components/_layouts/app'
+import { AuthLayout } from './components/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+
+  {
+    path: '/sign-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
 ])
